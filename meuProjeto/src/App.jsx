57ad40d1c79478d19e0cc777cd3b0d85c22d38e0
capprinha <gutoklein21@gmail.com>
2025-01-27 22:03:26 -0300

    add

---
+++ meuProjeto/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <form>
+        <label htmlFor="nomeUsuario">Nome:</label>
+        <input type="text" id="nomeUsuario" />
+
+        <label htmlFor="email">E-mail:</label>
+        <input type="email" id="email" />
+
+        <label htmlFor="senha">Senha:</label>
+        <input type="password" id="senha" />
+        <button type="submit">Confinar</button>
+        <button type="submit">Limpar</button>
+      </form>
+    </div>
+  );
+}
+export default App;
